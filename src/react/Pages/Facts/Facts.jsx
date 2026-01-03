91dc0955cff78e4429eebfbce9a1f73152e79b9a
Schnnuu <80274118+Schnnuu@users.noreply.github.com>
2026-01-03 13:03:34 +0100

    Changed Button logic and added a continue button to the Feedback page

--- src/react/Pages/Facts/Facts.jsx
+++ src/react/Pages/Facts/Facts.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-
 import { Stack, Button } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
 
 const Facts = () => {
   const facts = [
@@ -8,12 +8,14 @@ const Facts = () => {
     'Tobacco kills more than 7 million people each year, including an estimated 1.6 million non-smokers who are exposed to second-hand smoke'
   ]
 
-  const [fact, setFact] = React.useState('Click the button to learn a fun fact!')
-
   const getRandomFact = () => {
     const randomIndex = Math.floor(Math.random() * facts.length)
-    setFact(facts[randomIndex])
+    return facts[randomIndex]
   }
+
+  const [fact, setFact] = React.useState(() => getRandomFact())
+
+  const navigate = useNavigate()
 
   return (
     <Stack
@@ -25,14 +27,20 @@ const Facts = () => {
     >
       <h2>Smoking Facts</h2>
       <h3>Did you know?</h3>
-      <h4>
-        {fact}
-      </h4>
+      <h4>{fact}</h4>
+
       <Button
         variant="contained"
-        onClick={getRandomFact}
+        onClick={() => setFact(getRandomFact())}
       >
-        Show Me a Fun Fact
+        Give me another fact
+      </Button>
+      <br />
+      <Button
+        variant="outlined"
+        onClick={() => navigate('/Feedback')}
+      >
+        continue
       </Button>
     </Stack>
   )
