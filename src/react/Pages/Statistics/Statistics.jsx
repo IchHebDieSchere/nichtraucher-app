d3c8adde8f36bc0106ce3a17d4c9d3cf2ff276f1
IchHebDieSchere <lennart.koch@student.htw-berlin.de>
2026-01-08 12:09:47 +0100

    init

---
+++ src/react/Pages/Statistics/Statistics.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { Stack, Typography } from '@mui/material'
+
+const Statistics = () => (
+  <Stack
+    flex="1 1 auto"
+    justifyContent="center"
+    alignItems="center"
+  >
+    <Typography
+      variant="h4"
+    >
+      Welcome to the App
+    </Typography>
+  </Stack>
+)
+
+export default Statistics
