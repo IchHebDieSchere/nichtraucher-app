8915624a9f3ed5f2732ad41d7c79056864292b0f
Zidane Syukri <zimasupro@users.noreply.github.com>
2026-01-13 22:47:37 +0100

    feat: Add glow mode

--- src/react/Components/HomescreenWidget.jsx
+++ src/react/Components/HomescreenWidget.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Stack, Avatar } from "@mui/material";
+import { Stack, Avatar, Box } from "@mui/material";
 
 import { useState } from "react";
 
@@ -8,50 +8,74 @@ import { useNavigate } from "react-router-dom";
 
 const HomescreenWidget = () => {
   const [isPressed, setIsPressed] = useState(false);
+  const [isGlow, setIsGlow] = useState(false);
   const navigate = useNavigate();
 
   return (
-    <Stack
-      justifyContent="center"
-      alignItems="center"
-      sx={{
-        height: "166px",
-        backgroundColor: "#80808080", // Transparent Gray
-        borderRadius: (theme) => theme.shape.borderRadius,
-      }}
-    >
-      <Avatar
+    <Stack>
+      <Stack
+        justifyContent="center"
+        alignItems="center"
         sx={{
-          width: "150px",
-          height: "150px",
-          fontSize: "40px",
-          backgroundColor: "black",
-          transform: "rotateX(45deg)",
-        }}
-      ></Avatar>
-      <Avatar
-        onMouseUp={() => {
-          setTimeout(() => {
-            setIsPressed(true);
-          }, 100);
-          setTimeout(() => {
-            setIsPressed(false);
-          }, 300);
-          setTimeout(() => {
-            navigate("/");
-          }, 500);
-        }}
-        sx={{
-          width: "150px",
-          height: "150px",
-          fontSize: "40px",
-          transform: "rotateX(45deg)",
-          backgroundColor: "red",
-          marginTop: isPressed ? "-150px" : "-160px",
+          height: "166px",
+          backgroundColor: "#80808080", // Transparent Gray
+          borderRadius: (theme) => theme.shape.borderRadius,
+          position: "relative",
         }}
       >
-        HELP
-      </Avatar>
+        <Avatar
+          sx={{
+            width: "150px",
+            height: "150px",
+            fontSize: "40px",
+            backgroundColor: "black",
+            transform: "rotateX(45deg)",
+            boxShadow: isGlow ? "0 0 10px 5px red" : "",
+          }}
+        ></Avatar>
+        <Avatar
+          onMouseUp={() => {
+            setTimeout(() => {
+              setIsPressed(true);
+            }, 100);
+            setTimeout(() => {
+              setIsPressed(false);
+            }, 300);
+            setTimeout(() => {
+              navigate("/");
+            }, 500);
+          }}
+          sx={{
+            width: "150px",
+            height: "150px",
+            fontSize: "40px",
+            transform: "rotateX(45deg)",
+            backgroundColor: "red",
+            marginTop: isPressed ? "-150px" : "-160px",
+          }}
+        >
+          HELP
+        </Avatar>
+        <Box
+          sx={{
+            width: "20px",
+            height: "20px",
+            backgroundColor: isGlow ? "orange" : "",
+            marginTop: "-125px",
+            marginRight: "-125px",
+            position: "absolute",
+            borderRadius: "50%",
+          }}
+        ></Box>
+      </Stack>
+      <label>
+        <input
+          type="checkbox"
+          checked={isGlow}
+          onChange={(e) => setIsGlow(e.target.checked)}
+        ></input>
+        Glow Mode!
+      </label>
     </Stack>
   );
 };
