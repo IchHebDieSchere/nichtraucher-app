65ce41b31b3e8ab5b5bcaa13987509a85f17dbde
Zidane Syukri <zimasupro@users.noreply.github.com>
2026-01-13 21:44:44 +0100

    style: Add black base and press animation to HomescreenWidget

--- src/react/Components/HomescreenWidget.jsx
+++ src/react/Components/HomescreenWidget.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-import { Stack, Avatar, Typography } from "@mui/material";
+import { Stack, Avatar } from "@mui/material";
+
+import { useState } from "react";
 
 import { useNavigate } from "react-router-dom";
 
 const HomescreenWidget = () => {
+  const [isPressed, setIsPressed] = useState(false);
   const navigate = useNavigate();
 
   return (
@@ -18,12 +21,33 @@ const HomescreenWidget = () => {
       }}
     >
       <Avatar
-        onClick={() => navigate("/")}
         sx={{
           width: "150px",
           height: "150px",
           fontSize: "40px",
+          backgroundColor: "black",
+          transform: "rotateX(45deg)",
+        }}
+      ></Avatar>
+      <Avatar
+        onMouseUp={() => {
+          setTimeout(() => {
+            setIsPressed(true);
+          }, 100);
+          setTimeout(() => {
+            setIsPressed(false);
+          }, 300);
+          setTimeout(() => {
+            navigate("/");
+          }, 500);
+        }}
+        sx={{
+          width: "150px",
+          height: "150px",
+          fontSize: "40px",
+          transform: "rotateX(45deg)",
           backgroundColor: "red",
+          marginTop: isPressed ? "-150px" : "-160px",
         }}
       >
         HELP
