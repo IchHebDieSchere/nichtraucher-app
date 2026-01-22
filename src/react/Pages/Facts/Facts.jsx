666e0fe7ca1b79e07f36daec7f713fd3b7d0b897
Schnnuu <80274118+Schnnuu@users.noreply.github.com>
2026-01-22 18:14:10 +0100

    Added comments to Facts.jsx

--- src/react/Pages/Facts/Facts.jsx
+++ src/react/Pages/Facts/Facts.jsx
@@ -2,6 +2,11 @@ import React, { useState } from 'react'
 import { Stack, Button } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
 
+/**
+ * Used for filtering out used Elements in an Array: https://react.dev/learn/updating-arrays-in-state
+ * Used for getRandomFacts: https://stackoverflow.com/questions/62709706/how-do-i-get-a-random-element-from-an-array-when-a-button-is-clicked
+ */
+
 const allFacts = [
   { id: 0, fact: 'Tobacco kills up to half of its users who don’t quit' },
   { id: 1, fact: 'Tobacco kills more than 7 million people each year, including an estimated 1.6 million non-smokers who are exposed to second-hand smoke' },
@@ -15,11 +20,15 @@ const getRandomFact = (facts) => facts[Math.floor(Math.random() * facts.length)]
 const Facts = () => {
   const navigate = useNavigate()
 
+  // Random fact is being generated upon first loading the page from allFacts
   const [currentFact, setCurrentFact] = useState(() => getRandomFact(allFacts))
 
+  // Array of facts is being loaded, currently displayed facts is filtered out
   const [remainingFacts, setRemainingFacts] = useState(() => allFacts.filter(f => f.id !== currentFact.id))
 
   const showNextFact = () => {
+    // When all facts have been shown, new array is initialized,
+    // current fact is excluded to avoid showing same fact twice in a row
     if (remainingFacts.length === 0) {
       const freshPool = allFacts.filter(
         f => f.id !== currentFact.id
