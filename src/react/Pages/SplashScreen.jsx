60afcd70793e15e67e7f99ab2682e9e71f7b4625
valeriia <valeriia.ievseichyk@gmail.com>
2026-02-10 22:53:49 +0100

    add transition to splash screen

--- src/react/Pages/SplashScreen.jsx
+++ src/react/Pages/SplashScreen.jsx
@@ -1,9 +1,15 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Stack, Typography } from '@mui/material'
+import { Stack, Typography, Fade } from '@mui/material'
+import SmokeFreeIcon from '@mui/icons-material/SmokeFree'
 
 const SplashScreen = () => {
   const navigate = useNavigate()
+  const [fade, setFade] = useState(true)
+
+  useEffect(() => {
+    setFade(false)
+  }, [])
 
   useEffect(() => {
     const autoRedirectTimerId = setTimeout(() => {
@@ -20,7 +26,7 @@ const SplashScreen = () => {
       } else {
         navigate('/error')
       }
-    }, 1000)
+    }, 5000)
 
     return () => {
       clearTimeout(autoRedirectTimerId)
@@ -28,9 +34,15 @@ const SplashScreen = () => {
   }, [])
 
   return (
-    <Stack flex="1 1 auto" justifyContent="center" alignItems="center">
-      <Typography>RauchStopp</Typography>
-    </Stack>
+    <Fade appear={false} in={fade} timeout={5200}>
+      <Stack
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Typography marginBottom={1} variant="h4">RauchStopp</Typography>
+        <SmokeFreeIcon sx={{ fontSize: 55 }} />
+      </Stack>
+    </Fade>
   )
 }
 
