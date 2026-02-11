f3d021f0f58208170dff6d32cc8518410ceea185
IchHebDieSchere <lennart.koch39@gmail.com>
2026-02-11 18:41:06 +0100

    added username to main menu

--- src/react/Pages/MainMenu/MainMenu.jsx
+++ src/react/Pages/MainMenu/MainMenu.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-import { Stack, ToggleButtonGroup, ToggleButton } from '@mui/material'
+import { Stack, ToggleButtonGroup, ToggleButton, Typography } from '@mui/material'
 
 import Statistics from '../Statistics/Statistics'
 import Methods from '../Methods/Methods'
@@ -8,17 +8,33 @@ import useStats from '../../../hooks/useStats'
 import useMainMenuTab from '../../../hooks/useMainMenuTab'
 import useMethods from '../../../hooks/useMethods'
 
+const STORAGE_KEY = 'userName'
+
 const MainMenu = () => {
   const { tab, handleTabChange } = useMainMenuTab('statistik')
   const { stats, setStats, incrementMission } = useStats()
   const { selectedMethod, setSelectedMethod } = useMethods()
 
+  const [username, setUsername] = useState('')
+
+  useEffect(() => {
+    const savedName = localStorage.getItem(STORAGE_KEY)
+    if (savedName) {
+      setUsername(savedName)
+    }
+  }, [])
+
   return (
-    <Stack alignItems="center" spacing={10}>
+    <Stack alignItems="center" spacing={7}>
       <ToggleButtonGroup value={tab} exclusive onChange={handleTabChange}>
         <ToggleButton value="statistik">Statistics</ToggleButton>
         <ToggleButton value="methoden">Methods</ToggleButton>
       </ToggleButtonGroup>
+
+      <Typography variant="h5">
+        Hello, {username}!
+      </Typography>
+
 
       {tab === 'statistik' && (
         <Statistics stats={stats} setStats={setStats} incrementMission={incrementMission} selectedMethod={selectedMethod} setSelectedMethod={setSelectedMethod} />
